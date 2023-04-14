331928e83cb2ea35c8c560ece2dd20448cc96ea7
森史憲 <mori@codingdesing.jp>
2023-04-14 11:50:43 +0900

    XML読み込み＆表示を調整＆ソースの整理

--- pages/index.vue
+++ pages/index.vue
@@ -1,124 +1,96 @@
 <script setup lang="ts">
-const formatted = useDateWithTime(Date.now())
-// import { xml2js } from "xml2js";
-// データを取得
-// const cityName = 'kagoshima'
-// const APIkey = 'bec7720eb6429bd39f103573d1af2894'
-// const { data: weather, error } = await useFetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIkey}&lang=ja`)
-
-
-  // useFetch用の設定
-  let contents = ''
-  await useFetch('http://localhost:3000/blog.xml')
-    .then((response) => {
-      console.log('response')
-      let strXml = ''
-      // console.log(response.data.value)
-      if (typeof response.data.value === 'string') {
-        strXml = response.data.value
-      }
-      const parser = new DOMParser();
-      let xmlData  = parser.parseFromString(strXml, "text/xml");
-      let xmlDataItems = xmlData.querySelectorAll('item')
-      console.log(xmlData);
-      // console.log(xmlData.querySelectorAll('item'));
-      // console.log(xmlData0.querySelector('title')!.textContent);
-
-      xmlDataItems.forEach(item => {
-        contents +=
-        `
-          <article class="article-list__item">
-            <div class="article-list__date">
-              ${ item.querySelector('dc\\:date')!.textContent }
-            </div>
-            <h2 class="article-list__title">
-              <a
-                href="${ item.querySelector('link')!.textContent }"
-                target="_blank"
-                class="article-list__title-link"
-              >
-                ${ item.querySelector('title')!.textContent }
-              </a>
-            </h2>
-            <div class="article-list__publisher">
-              <p class="article-list__publisher-text">
-                From <a href="${ item.querySelector('dc\\:identifier')!.textContent }"
-                target="_blank"
-                class="article-list__publisher-link"
-                >
-                  ${ item.querySelector('dc\\:publisher')!.textContent }
-                </a>
-              </p>
-            </div>
-          </article>
-        `
-      });
-      // console.log(contents)
-
-      // let jsonData;
-      // XMLをJSONに変換するオブジェクトのインスタンスを作成
-      // async: 非同期かどうか
-      // explicitArray: trueの場合、常に子ノードを配列に配置。それ以外の場合、配列は複数ある場合にのみ作成
-      // const parser = new xml2js.Parser({
-      //   async: false,
-      //   explicitArray: false,
-      //   trim: true,
-      // });
-      // console.log(parser)
-      // 変換を実行
-      // data: 取得したxml
-      // json: 変換結果(jsonデータ)
-      // parser.parseString(response.data.value, (error, json) => {
-      //   jsonData = json;
-      // });
-      // console.log(jsonData)
-      // transformResponseの実行結果を戻す
-      // return jsonData;
-
-      // contents = response.data["rdf:RDF"].item
-    })
-    .catch((e) => console.error(e));
+// XMLの読み込み
+let contents = "";
+const { data } = await useFetch("http://localhost:3000/blog.xml");
+let strXml = "";
+// データの型チェック
+if (typeof data.value === "string") {
+  strXml = data.value;
+}
+// DOMはクライアント側でのみ処理するため、process.clientで処理を切り分け
+if (process.client) {
+  // 文字列をXMLとして読めるようにする
+  const parser = new DOMParser();
+  let xmlData = parser.parseFromString(strXml, "text/xml");
+  let xmlDataItems = xmlData.querySelectorAll("item");
+  // 画面表示用のHTMLを組み立てる
+  xmlDataItems.forEach((item) => {
+    contents += `
+      <article class="article-list__item">
+        <div class="article-list__date">
+          ${useDateWithTime(item.querySelector("date")!.textContent).value}
+        </div>
+        <h2 class="article-list__title">
+          <a
+            href="${item
+              .querySelector("link")!
+              .textContent?.replace(
+                "http://www.synapse.jp/blog/cgi/go.cgi?url=",
+                ""
+              )}"
+            target="_blank"
+            class="article-list__title-link"
+          >
+            ${item.querySelector("title")!.textContent}
+          </a>
+        </h2>
+        <div class="article-list__publisher">
+          <p class="article-list__publisher-text">
+            From <a href="${item.querySelector("identifier")!.textContent}"
+            target="_blank"
+            class="article-list__publisher-link"
+            >
+              ${item.querySelector("publisher")!.textContent}
+            </a>
+          </p>
+        </div>
+      </article>
+    `;
+  });
+}
 </script>
 
 <template>
   <div class="wrapper">
     <h1 id="top">トップページ</h1>
     <p>
-      <span>これは</span><span>テスト</span><span>です</span><br>
+      <span>これは</span><span>テスト</span><span>です</span><br />
       <NuxtLink to="/blog">blogページ</NuxtLink>
     </p>
     <div>
       <!-- この`to`はNuxtLinkのパラメーターで`href`とほぼ同じ役割だが、`href`がないとスクロールされないのでちゃんと両方書く -->
       <NuxtLink to="#scroll">スクロールする</NuxtLink>
     </div>
-    <p>アイコンの表示<br>
+    <p>
+      アイコンの表示<br />
       <nuxt-icon name="chat" class="icon-chat" />
     </p>
     <!--
-      ブラウザの状態によって表示が変わるところは
-      client-onlyコンポーネントを使わないとwarningが表示される
+      windowオブジェクトやdocumentオブジェクトなどブラウザの機能を使うところは
+      client-onlyコンポーネントで囲う
     -->
     <client-only>
-      <div>現在時刻の表示<br>
-        {{ formatted }}</div>
       <div>
-        メディアクエリの判定<br>
-        大きい画面か？: {{ useMQ() }}<br>
+        メディアクエリの判定<br />
+        大きい画面か？: {{ useMQ() }}<br />
       </div>
+      <p>XMLを読み込んで表示</p>
+      <div class="article-list" v-html="contents"></div>
     </client-only>
-    <div class="article-list" v-html="contents"></div>
-    <!-- {{ contents }} -->
-    <!-- <h1>{{ weather.name }}</h1>
-    <p>{{ weather.weather[0].description }}</p>
-    <p>{{ weather.main.temp }} °C</p> -->
-    <div id="scroll">ようこそ　<NuxtLink to="#top">ページの先頭へ戻る</NuxtLink></div>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat non rerum illum unde, error veritatis sit voluptatibus tenetur perferendis eum vero temporibus similique quaerat tempora repudiandae! Delectus porro vitae id.</p>
+    <div id="scroll">
+      ようこそ　<NuxtLink to="#top">ページの先頭へ戻る</NuxtLink>
+    </div>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat non rerum
+      illum unde, error veritatis sit voluptatibus tenetur perferendis eum vero
+      temporibus similique quaerat tempora repudiandae! Delectus porro vitae id.
+    </p>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .wrapper {
-  width: math.div(80,100)*100%;
+  width: math.div(80, 100) * 100%;
 }
 p {
   span {
@@ -134,11 +106,11 @@ p {
   }
 }
 #scroll {
-	margin-top: 1500px;
+  margin-top: 1500px;
 }
 @include for-sp {
   p {
-  span {
+    span {
       &:nth-child(1) {
         color: pink;
       }
@@ -147,7 +119,7 @@ p {
 }
 @include for-pc {
   p {
-  span {
+    span {
       &:nth-child(1) {
         color: skyblue;
       }
